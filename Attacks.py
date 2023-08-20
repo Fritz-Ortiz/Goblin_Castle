d0d7d209dda48753a1c7bd4a178bf892c39fecd9
Fritz Ortiz <fritzortiz.27@gmail.com>
2023-08-20 19:29:36 -0400

    First concept demo of basic interactions

---
+++ Attacks.py
@@ -0,0 +1,11 @@
+import random
+
+def Light_Punch():
+    damage = random.randint(2,4)
+    return damage
+def Shortsword_Slash():
+    damage = random.randint(9,15)
+    return damage
+def Sick_bite():
+    damage = random.randint(1,10)
+    return damage
---
+++ Encounter.py
@@ -0,0 +1,9 @@
+from Monsters import *
+
+ready = input("Are you ready to fight? (Y/N) ")
+
+if ready == "Y":
+    print("You encounter a goblin with " + str(Goblin.Life_Points()) + " Hit Points. It attacks and " + Goblin.attacks())
+
+else:
+    print("Coward")
