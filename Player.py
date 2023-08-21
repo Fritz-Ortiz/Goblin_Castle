5a0eeb9bcb60c1df0663b253a0e33d74f1cf5fc9
Fritz Ortiz <fritzortiz.27@gmail.com>
2023-08-21 00:34:33 -0400

    Changed print statements to return statements for proper function

--- Player.py
+++ Player.py
@@ -1,13 +1,17 @@
+#from Character_Creation import *
 class stats:
     def Hit_Points():
         Health = 100
-        return Health  
+        return Health
     def Dexterity():
         Dex = 10
         return Dex
     def Strength():
         stren = 10
+        return stren
     def Constitution():
         Cons = 10
+        return Cons
     def Fate():
-        luck = 10    
+        luck = 10
+        return luck   
