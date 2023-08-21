15091c0d3369dc169cb1a23bec0b90c136eebd94
Fritz Ortiz <fritzortiz.27@gmail.com>
2023-08-21 00:53:22 -0400

    Much better way of getting and modifying stats

--- Player.py
+++ Player.py
@@ -1,17 +1,29 @@
 #from Character_Creation import *
-class stats:
-    def Hit_Points():
-        Health = 100
-        return Health
-    def Dexterity():
-        Dex = 10
-        return Dex
-    def Strength():
-        stren = 10
-        return stren
-    def Constitution():
-        Cons = 10
-        return Cons
-    def Fate():
-        luck = 10
-        return luck   
+class Stats:
+    def __init__(self):
+        self.attributes = {
+            'Hit Points': 100,
+            'Dexterity': 10,
+            'Strength': 10,
+            'Constitution': 10,
+            'Fate': 10
+        }
+
+    def get_attribute(self, attribute_name):
+        return self.attributes.get(attribute_name)
+
+    def set_attribute(self, attribute_name, value):
+        self.attributes[attribute_name] = value
+
+
+
+#stats = Stats()
+
+# Get the value of an attribute
+#hit_points = stats.get_attribute('Hit Points')
+#print("Hit Points:", hit_points)
+
+# Set the value of an attribute
+#stats.set_attribute('Dexterity', 15)
+#dexterity = stats.get_attribute('Dexterity')
+#print("Dexterity:", dexterity)
